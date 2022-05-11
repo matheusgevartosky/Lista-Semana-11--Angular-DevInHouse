24d995559762742d2c93e4b2c962c937882e4fd7
matheusgevartosky <magevartosky@gmail.com>
2022-05-11 11:35:39 -0300

    Add Exercicio 1

---
+++ src/app/classes/star-wars.spec.ts
@@ -0,0 +1,7 @@
+import { StarWars } from './star-wars';
+
+describe('StarWars', () => {
+  it('should create an instance', () => {
+    expect(new StarWars()).toBeTruthy();
+  });
+});
---
+++ src/app/classes/star-wars.ts
@@ -0,0 +1,20 @@
+export class StarWars {
+  id:string;
+  name:string;
+  habilities:string;
+  planet:string;
+  wapons:string;
+  avatar:string;
+
+    constructor(id:string,name:string,habilities:string, planet:string,wapons:string, avatar:string){
+      this.id = id
+      this.name = name
+      this.habilities = habilities
+      this.planet = planet
+      this.avatar = avatar
+      this.wapons = wapons
+
+    }
+
+
+}
